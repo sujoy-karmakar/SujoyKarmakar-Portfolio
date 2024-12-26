a47bf56982ab14625e4fbabb0441ce32a2001923
sujoy-karmakar <ksujoy29@gmail.com>
2024-12-26 22:03:50 +0530

    my portfollio website

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
